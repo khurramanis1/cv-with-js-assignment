04509b6bc9e0ffeb72207a10381f1b5037d213ac
Muhammad Khurram Anis <muhammadkhurramanis@gmail.com>
2025-09-14 08:03:35 +0500

    Simple CV by using JS

---
+++ index.js
@@ -0,0 +1,51 @@
+let img = document.getElementById("img");
+function openImg() {
+    img.style.border = "2px solid #333";
+    img.style.width = "250px";
+    img.style.height = "312px";
+    let pic = document.createElement("img");
+    pic.src = "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-881954-2379005.jpg&fm=jpg";
+    pic.style.width = "100%";
+    pic.style.height = "100%";
+    pic.style.objectFit = "cover";
+    pic.classList.add("manPic");
+    img.innerHTML = "";
+    img.appendChild(pic);
+}
+
+//  introduction part
+let intro = document.getElementById("intro");
+let introSec = document.getElementById("introSec");
+introSec.style.display = "none";
+function openIntro() {
+    introSec.style.display = "block";
+    intro.innerHTML = "";
+    intro.appendChild(introSec)
+};
+
+//  contact form part
+let contact = document.getElementById("contact");
+let contactForm = document.getElementById("contactForm");
+contactForm.style.display = "none";
+function seeContact() {
+    contactForm.style.display = "flex";
+    contact.innerHTML = "";
+    contact.appendChild(contactForm);
+}
+
+//  function for submission
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+function submitForm(event){
+    event.preventDefault();
+    if(name.value.trim() === ""){
+        alert("Please enter your name")
+    }
+    else if(email.value.trim() === ""){
+        alert("Please enter an email address");
+    }
+    else {
+        alert("Thanks for submission!");
+    }
+}
